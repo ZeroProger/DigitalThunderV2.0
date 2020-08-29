c97c7b9c2da479f0b08f5590ddd6a4649e1ef9db
ZeroProger <56200967+ZeroProger@users.noreply.github.com>
2020-08-29 21:15:24 +0500

    'test'

--- app/src/main/java/com/example/digitalthunder/MainActivity.java
+++ app/src/main/java/com/example/digitalthunder/MainActivity.java
@@ -10,7 +10,7 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 import java.sql.*;
-
+//Test commit
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     Button Autoris_but;
     TextView textView_name;

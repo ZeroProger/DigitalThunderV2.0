6f456e69cfd3ea2f2521d3efa6844812df98c875
HevyPunk <kirillkuchma8@gmail.com>
2020-08-29 22:22:21 +0500

    Добавил обратотку XML

--- app/src/main/java/com/example/digitalthunder/MainMenu.java
+++ app/src/main/java/com/example/digitalthunder/MainMenu.java
@@ -5,7 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class MainMenu extends AppCompatActivity {
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+
+public class MainMenu extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +28,21 @@ public class MainMenu extends AppCompatActivity {
         Bundle args = getIntent().getExtras();
         TextView UsFirstName = (TextView) findViewById(R.id.user_name);
         TextView UsSecondName = (TextView) findViewById(R.id.user_second_name);
+        TextView FileStatus = (TextView) findViewById(R.id.databaseStatus);
 
         UsFirstName.setText(args.getCharSequence("User name"));
         UsSecondName.setText(args.getCharSequence("User second name"));
+
+        try{
+            DocumentBuilder xml = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            Document doc = xml.parse(new File("database.xml"));
+            System.out.println(doc.getDocumentElement());
+            Element rootel = doc.getDocumentElement();
+            FileStatus.setText(rootel.getNodeName());
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
     }
+
 }

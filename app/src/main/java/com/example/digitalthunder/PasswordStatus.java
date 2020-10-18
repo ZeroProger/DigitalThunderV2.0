23b30caa223ce79d491d6b59a28f97bb7f02009d
ZeroProger <56200967+ZeroProger@users.noreply.github.com>
2020-10-19 00:05:36 +0500

    .

--- app/src/main/java/com/example/digitalthunder/PasswordStatus.java
+++
@@ -1,7 +0,0 @@
-package com.example.digitalthunder;
-
-public enum PasswordStatus {
-    SUCCESSFUL,
-    PASSWORD_NOT_FOUND,
-    INCORRECT_PASSWORD
-}
--- app/src/main/java/com/example/digitalthunder/VerifyDatabaseStat.java
+++
@@ -1,7 +0,0 @@
-package com.example.digitalthunder;
-
-public enum VerifyDatabaseStat {
-    HUMAN_IS_WRONG,
-    COMPLETED,
-    ERROR
-}
